54666d0cb00c34155935afec3ffc5a7b5216dd8b
Miran <222miran222@gmail.com>
2024-07-02 18:36:31 +0530

    Health Checkup route

--- api/index.js
+++ api/index.js
@@ -40,6 +40,11 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
 })
 
+// Health check endpoint
+app.get('/health', (req, res) => {
+    res.status(200).send('OK');
+});
+
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
     const message = err.message || 'Internal Server Error';
